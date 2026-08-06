b44bd5e4bc410bfd7307b134426ec9c52573abe7
rebiz <1296550727652610189+rebizzz@users.noreply.github.com>
2026-08-06 19:54:18 +0530

    discord: revert invalid nixcord plugin options

--- modules/home/apps/discord.nix
+++ modules/home/apps/discord.nix
@@ -8,7 +8,6 @@
         branch = "canary";
         equicord.enable = true;
         krisp.enable = true;
-        openASAR.enable = true;
       };
 
       config = {
@@ -19,82 +18,6 @@
         enabledThemeLinks = [
           "https://raw.githubusercontent.com/LuckFire/amoled-cord/343808e7d5297223e43868b3955da4cbbd01ceef/clients/amoled-cord.theme.css"
         ];
-
-        plugins = {
-          # UI / QoL
-          accountPanelServerProfile.enable = true;
-          advancedPermissions.enable = true;
-          alwaysExpandProfiles.enable = true;
-          alwaysTrust.enable = true;
-          anonymiseFileNames.enable = true;
-          autoZipper.enable = true;
-          betterActivities.enable = true;
-          betterBanReasons.enable = true;
-          betterForwards.enable = true;
-          betterInvites.enable = true;
-          betterRoleContext.enable = true;
-          betterSessions.enable = true;
-          betterSettings.enable = true;
-          betterUploadButton.enable = true;
-          biggerStreamPreview.enable = true;
-          bypassPinPrompt.enable = true;
-          callTimer.enable = true;
-          clickableRoles.enable = true;
-          clipsEnhancements.enable = true;
-          clipUpload.enable = true;
-          collapsibleUI.enable = true;
-          copyFileContents.enable = true;
-          copyStatusUrls.enable = true;
-          copyUserMention.enable = true;
-          downloadAllAttachments.enable = true;
-          expressionCloner.enable = true;
-          fakeNitro.enable = true;
-          findReply.enable = true;
-          fixImagesQuality.enable = true;
-          forceOwnerCrown.enable = true;
-          fullVCPFP.enable = true;
-          iLoveSpam.enable = true;
-          imageFilename.enable = true;
-          imageZoom.enable = true;
-          iRememberYou.enable = true;
-          limitlessScreenshare.enable = true;
-          memberCount.enable = true;
-          messageClickActions.enable = true;
-          messageLogger.enable = true;
-          messageLoggerEnhanced.enable = true;
-          mutualGroupDMs.enable = true;
-          noNitroUpsell.enable = true;
-          noTypingAnimation.enable = true;
-          onePingPerDM.enable = true;
-          permissionsViewer.enable = true;
-          platformIndicators.enable = true;
-          relationshipNotifier.enable = true;
-          serverInfo.enable = true;
-          showAllMessageButtons.enable = true;
-          showHiddenChannels.enable = true;
-          showHiddenThings.enable = true;
-          showSongName.enable = true;
-          showTimeoutDuration.enable = true;
-          silentTyping.enable = true;
-          sortFriends.enable = true;
-          summaries.enable = true;
-          timezones.enable = true;
-          typingTweaks.enable = true;
-          universalMention.enable = true;
-          unlockedAvatarZoom.enable = true;
-          userVoiceShow.enable = true;
-          validReply.enable = true;
-          validUser.enable = true;
-          vcPanelSettings.enable = true;
-          viewIcons.enable = true;
-          voiceChatDoubleClick.enable = true;
-          voiceDownload.enable = true;
-          voiceStats.enable = true;
-          volumeBooster.enable = true;
-          whoReacted.enable = true;
-          whosWatching.enable = true;
-          zipPreview.enable = true;
-        };
       };
     };
   };
