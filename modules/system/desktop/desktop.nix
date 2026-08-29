649f245cd242965f60d118cde9bb9b686370ce35
rebiz <1296550727652610189+rebizzz@users.noreply.github.com>
2026-08-29 21:21:08 +0530

    fix(desktop): use top-level pkgs for thunar plugins

--- modules/system/desktop/desktop.nix
+++ modules/system/desktop/desktop.nix
@@ -22,7 +22,7 @@
 
       thunar = {
         enable = true;
-        plugins = with pkgs.xfce; [
+        plugins = with pkgs; [
           thunar-archive-plugin
           thunar-volman
           thunar-media-tags-plugin
