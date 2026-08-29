237a6674f4e88ad8330b36ef4f59e4ad5c3f3a24
rebiz <1296550727652610189+rebizzz@users.noreply.github.com>
2026-08-29 21:23:48 +0530

    feat(noctalia): enable gtk3 theme template for Thunar

--- modules/home/desktop/noctalia.nix
+++ modules/home/desktop/noctalia.nix
@@ -96,7 +96,7 @@ _: {
           wallpaper_scheme = "m3-content";
           templates = {
             enable_builtin_templates = true;
-            builtin_ids = ["kitty" "umbriel"];
+            builtin_ids = ["kitty" "gtk3" "umbriel"];
             enable_community_templates = true;
             community_ids = ["fastfetch" "bat"];
           };
