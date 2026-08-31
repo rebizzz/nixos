6588d890b6c9519548a925002d8b8c06f1de0b55
rebiz <1296550727652610189+rebizzz@users.noreply.github.com>
2026-08-31 14:32:55 +0530

    fix(boot): remove CRYPTO_USER_API modules from blacklist to fix iwd Wi-Fi daemon

--- modules/system/core/boot.nix
+++ modules/system/core/boot.nix
@@ -21,13 +21,6 @@ _: {
       "jffs2"
       "hfs"
       "hfsplus"
-
-      # Fedora 45 / modern kernel security hardening: Disable in-kernel Crypto Userspace API (CRYPTO_USER_API)
-      "af_alg"
-      "algif_hash"
-      "algif_skcipher"
-      "algif_rng"
-      "algif_aead"
     ];
   in {
     boot = {
