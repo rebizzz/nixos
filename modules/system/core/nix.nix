77365d86ca4a5ea9c9c925cfb4344cc811346439
rebiz <1296550727652610189+rebizzz@users.noreply.github.com>
2026-09-07 19:20:04 +0530

    chore: align automatic gc retention (2d -> 7d) with manual just gc

--- modules/system/core/nix.nix
+++ modules/system/core/nix.nix
@@ -63,7 +63,7 @@
       gc = {
         automatic = true;
         dates = "daily";
-        options = "--delete-older-than 2d";
+        options = "--delete-older-than 7d";
       };
 
       daemonCPUSchedPolicy = "batch";
